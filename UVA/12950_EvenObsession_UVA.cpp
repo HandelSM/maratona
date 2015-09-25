1b35d8c49be941d9235f370aea4cdc711bcfb2da
Handel Scholze Marques <handel@guybrush.lablua>
2015-09-24 22:31:28 -0300

    Comecando a fazer problema A da maratona

---
+++ UVA/12950_EvenObsession_UVA.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <vector>
+#include <queue>
+
+
+typedef struct
+{
+	int v;
+	int c;
+}Edge;
+
+typedef struct
+{
+	int v;
+	int c;
+	int pai;
+	int len;
+}Path;
+
+int main()
+{
+
+	while(scanf("%d %d", &n, &e)!=EOF)
+	{
+
+
+
+
+
+	}
+	return 0;
+}
